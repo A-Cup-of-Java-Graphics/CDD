741b6ab89f13a48f7836d0695488f9bf97421439
Youlin Huang <codecovii@gmail.com>
2024-07-10 16:58:49 -0400

    Clean up Window class
    Normally importing * is disliked, since it is likely to cause conflicting imports, but since all of GLFW's shit starts with glfw, it kinda evades that problem, unless there is some other method starting with glfw, which in most cases is never even gonna happen anyways.

--- CatNipGame/app/src/main/java/CDD/Window.java
+++ CatNipGame/app/src/main/java/CDD/Window.java
@@ -1,24 +1,6 @@
 package CDD;
 
-import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-//import static org.lwjgl.glfw.GLFW.glfwGetWindowMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-//import static org.lwjgl.glfw.GLFW.glfwSetWindowIcon;
-//import static org.lwjgl.glfw.GLFW.glfwSetWindowMonitor;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowSize;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 //import static org.lwjgl.opengl.GL11.GL_DEPTH;
 //import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
