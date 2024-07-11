56168411a3c9596323c6eabb60f92cd1df038aa1
Ewo Jima <ll-EwoJima-ll@outlook.com>
2024-07-10 20:13:28 -0400

    Error fixes & New Features

--- CatNipGame/app/src/main/java/CDD/Input.java
+++ CatNipGame/app/src/main/java/CDD/Input.java
@@ -1,32 +1,33 @@
 package CDD;
-import static org.lwjgl.glfw.GLFW.glfwGetKey;
+import java.nio.DoubleBuffer;
 
-import CDDPhysics.EnumKeys;
-import CDDPhysics.Scene;
+import org.lwjgl.BufferUtils;
 
 import static org.lwjgl.glfw.GLFW.*;
 
 @SuppressWarnings("unused")
 public class Input {
-    public static boolean KeyPressed(long WindowHandle, int Key) {
+    public static int KeyPressed(long WindowHandle, int Key) {
         // If this returns 0, then the key was released
         // If this returns 1, then the key was pressed
         // If this returns 2, then the key was held
 
         int KeyReturned = glfwGetKey(WindowHandle, Key);
-        return KeyReturned == GLFW_TRUE;
+        return KeyReturned;
     }
 
-    public static void handleInputs(long window, Scene scene){
-        if(KeyPressed(window, scene.getSettings().getKeyFor(EnumKeys.MOVE_FORWARD))){
-            scene.getSettings().hitKey(EnumKeys.MOVE_FORWARD).accept(scene);;
-        }else if(KeyPressed(window, scene.getSettings().getKeyFor(EnumKeys.MOVE_BACKWARD))){
-            scene.getSettings().hitKey(EnumKeys.MOVE_BACKWARD).accept(scene);;
-        }
-        if(KeyPressed(window, scene.getSettings().getKeyFor(EnumKeys.MOVE_LEFT))){
-            scene.getSettings().hitKey(EnumKeys.MOVE_LEFT).accept(scene);;
-        }else if(KeyPressed(window, scene.getSettings().getKeyFor(EnumKeys.MOVE_RIGHT))){
-            scene.getSettings().hitKey(EnumKeys.MOVE_RIGHT).accept(scene);;
-        }
+    public static int Mouse(long WindowHandle, int MouseButton) {
+        glfwSetInputMode(WindowHandle, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        int MouseReturned = glfwGetMouseButton(WindowHandle, MouseButton);
+        //glfwSetMouseButtonCallback(WindowHandle, null)
+
+        return 0;
+    }
+
+    public static void GetMousePos(long WindowHandle) {
+        DoubleBuffer b1 = BufferUtils.createDoubleBuffer(1);
+        DoubleBuffer b2 = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(WindowHandle, b1, b2);
+        System.out.println("x : " + b1.get(0) + ", y = " + b2.get(0));
     }
 }
---
+++ CatNipGame/app/src/main/java/CDD/Monitor.java
@@ -0,0 +1,9 @@
+package CDD;
+
+import static org.lwjgl.glfw.GLFW.glfwGetMonitors;
+
+public class Monitor {
+    public static void GetMonitors() {
+        glfwGetMonitors();
+    }
+}
