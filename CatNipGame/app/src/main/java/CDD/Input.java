5c3f93a382f835474661a1622db52b4759d0f430
Ewo Jima <ll-EwoJima-ll@outlook.com>
2024-07-11 16:16:23 -0400

    Input Function Update

--- CatNipGame/app/src/main/java/CDD/Input.java
+++ CatNipGame/app/src/main/java/CDD/Input.java
@@ -47,4 +47,19 @@ public class Input {
         return gety.get();
     
     }
+
+    public static double[] GetMousePOS(long WindowHandle) {
+        glfwSetInputMode(WindowHandle, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        DoubleBuffer getx = BufferUtils.createDoubleBuffer(1);
+        DoubleBuffer gety = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(WindowHandle, getx, gety);
+
+        // xy[0] returns the x
+        // xy[1] returns the y
+        double[] xy = {getx.get(), gety.get()};
+
+        // Write The data
+        return xy;
+    
+    }
 }
